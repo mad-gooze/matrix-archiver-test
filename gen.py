ff72501a0311639480012cfa429c76fc8e268953
K900 <k0009000@gmail.com>
2014-06-26 13:03:38 +0400

    Python cleanup
    2to3, future imports for 2, code cleanup, some refactoring, @madgooze
    cannot into Python <3

--- gen.py
+++ gen.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+from __future__ import print_function
+from __future__ import division
 
 import numpy
 import shutil
@@ -11,111 +13,91 @@ MAX_MATRIX_SIZE = 1000
 MATRIX_INT_MIN = -10000
 MATRIX_INT_MAX = 10000
 
-print "Test matrix generator script for AESC Summer School Contest"
+print("Test matrix generator script for AESC Summer School Contest")
 
 def random_size():
-	return numpy.random.randint(MIN_MATRIX_SIZE, MAX_MATRIX_SIZE + 1)
+    return numpy.random.randint(MIN_MATRIX_SIZE, MAX_MATRIX_SIZE + 1)
 
 def gen_random(N):
-	matrix = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N, N))
-	return matrix
+    matrix = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N, N))
+    return matrix
 
 def gen_diagonal(N):
-	diagonal = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N))
-	return numpy.diag(diagonal)
+    diagonal = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N))
+    return numpy.diag(diagonal)
 
 def gen_triangular(N):
-	matrix = gen_random(N)
-	if numpy.random.choice([True, False]):
-		return numpy.triu(matrix)
-	else:
-		return numpy.tril(matrix)
+    matrix = gen_random(N)
+    if numpy.random.choice([True, False]):
+        return numpy.triu(matrix)
+    else:
+        return numpy.tril(matrix)
 
 def gen_symmetric(N):
-	matrix = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N, N))
-	matrix = (matrix + matrix.T)/2
-	return matrix
+    matrix = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N, N))
+    matrix = (matrix + matrix.T)/2
+    return matrix
 
 def gen_skew_symmetric(N):
-	matrix = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N, N))
-	matrix = numpy.triu(matrix)
-	matrix = matrix - matrix.T
-	return matrix
+    matrix = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N, N))
+    matrix = numpy.triu(matrix)
+    matrix = matrix - matrix.T
+    return matrix
 
 def gen_sparse(N):
-	matrix = numpy.zeros(shape = (N, N))
-	for index in numpy.ndenumerate(matrix):
-		if numpy.random.choice([True, False], p = [0.1, 0.9]):
-			matrix[index[0]] = numpy.random.randint(MATRIX_INT_MIN, MATRIX_INT_MAX + 1)
-	return matrix
+    matrix = numpy.zeros(shape = (N, N))
+    for index in numpy.ndenumerate(matrix):
+        if numpy.random.choice([True, False], p = [0.1, 0.9]):
+            matrix[index[0]] = numpy.random.randint(MATRIX_INT_MIN, MATRIX_INT_MAX + 1)
+    return matrix
 
 def gen_toeplitz(N):
-	c = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N))
-	r = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N))
-	# from scipy.linalg.toeplitz
-	c = numpy.asarray(c).ravel()
-	if r is None:
-		r = c.conjugate()
-	else:
-		r = numpy.asarray(r).ravel()
-	# Form a 1D array of values to be used in the matrix, containing a reversed
-	# copy of r[1:], followed by c.
-	vals = numpy.concatenate((r[-1:0:-1], c))
-	a, b = numpy.ogrid[0:len(c), len(r) - 1:-1:-1]
-	indx = a + b
-	# `indx` is a 2D array of indices into the 1D array `vals`, arranged so
-	# that `vals[indx]` is the Toeplitz matrix.
-	return vals[indx]
+    c = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N))
+    r = numpy.random.random_integers(MATRIX_INT_MIN, MATRIX_INT_MAX, size = (N))
+    # from scipy.linalg.toeplitz
+    c = numpy.asarray(c).ravel()
+    if r is None:
+        r = c.conjugate()
+    else:
+        r = numpy.asarray(r).ravel()
+    # Form a 1D array of values to be used in the matrix, containing a reversed
+    # copy of r[1:], followed by c.
+    vals = numpy.concatenate((r[-1:0:-1], c))
+    a, b = numpy.ogrid[0:len(c), len(r) - 1:-1:-1]
+    indx = a + b
+    # `indx` is a 2D array of indices into the 1D array `vals`, arranged so
+    # that `vals[indx]` is the Toeplitz matrix.
+    return vals[indx]
 
 def save_matrix(matrix, filename):
-	file = open(os.path.join(TESTS_DIR, filename), 'w')
-	print >> file, matrix.shape[0]
-	for row in matrix:
-		for cell in row:
-			print >> file, cell,
-		else:
-			print >> file
-	file.close()
+    with open(os.path.join(TESTS_DIR, filename), 'w') as file:
+        print(matrix.shape[0], file=file)
+        for row in matrix:
+            for cell in row:
+                print(cell, end=' ', file=file)
+            else:
+                print(file=file)
+
+if len(sys.argv) < 2:
+    sys.exit('Usage: {} n\nn - number of matrices of each type to be generated'.format(sys.argv[0]))
+
+N = int(sys.argv[1])
 
 shutil.rmtree(TESTS_DIR, ignore_errors = True)
 os.mkdir(TESTS_DIR)
 
-if len(sys.argv) < 2:
-    sys.exit('Usage: %s n\nn - number of matrices of each type to be generated' % sys.argv[0])
-N = int(sys.argv[1])
+generators = {
+    'random': gen_random,
+    'diagonal': gen_diagonal,
+    'triangular': gen_triangular,
+    'symmetric': gen_symmetric,
+    'skew_symmetric': gen_skew_symmetric,
+    'sparse': gen_sparse,
+    'toeplitz': gen_toeplitz
+}
 
-for i in range(N):
-	size = random_size()
-	matrix = gen_random(size)
-	save_matrix(matrix, "random_" + str(i) + ".txt")
-print "random generated"
-for i in range(N):
-	size = random_size()
-	matrix = gen_diagonal(size)
-	save_matrix(matrix, "diagonal_" + str(i) + ".txt")
-print "diagonal generated"
-for i in range(N):
-	size = random_size()
-	matrix = gen_triangular(size)
-	save_matrix(matrix, "triangular_" + str(i) + ".txt")
-print "triangular generated"
-for i in range(N):
-	size = random_size()
-	matrix = gen_symmetric(size)
-	save_matrix(matrix, "symmetric_" + str(i) + ".txt")
-print "symmetric generated"
-for i in range(N):
-	size = random_size()
-	matrix = gen_skew_symmetric(size)
-	save_matrix(matrix, "skew_symmetric_" + str(i) + ".txt")
-print "skew symmetric generated"
-for i in range(N):
-	size = random_size()
-	matrix = gen_sparse(size)
-	save_matrix(matrix, "sparse_" + str(i) + ".txt")
-print "sparse generated"
-for i in range(N):
-	size = random_size()
-	matrix = gen_toeplitz(size)
-	save_matrix(matrix, "toeplitz_" + str(i) + ".txt")
-print "toeplitz generated"
+for mtype, generator in generators.items():
+    for i in range(N):
+        matrix = generator(random_size())
+        save_matrix(matrix, "{}_{}.txt".format(mtype, i))
+    print("{} generated".format(mtype))
